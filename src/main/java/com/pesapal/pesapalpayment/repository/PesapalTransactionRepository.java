e239cd12b2bc9a3522ba035d29139ecbe6d4bf74
wilsonnjoroge <wilsonnjoroge932@gmail.com>
2026-03-04 20:07:15 +0300

    Changed the methods to return optional

--- src/main/java/com/pesapal/pesapalpayment/repository/PesapalTransactionRepository.java
+++ src/main/java/com/pesapal/pesapalpayment/repository/PesapalTransactionRepository.java
@@ -1,15 +1,14 @@
 package com.pesapal.pesapalpayment.repository;
 
 import com.pesapal.pesapalpayment.entity.PesapalTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+public interface PesapalTransactionRepository
+        extends JpaRepository<PesapalTransaction, Long> {
 
+    Optional<PesapalTransaction> findByPesapalOrderId(String pesapalOrderId);
 
-@Repository
-public interface PesapalTransactionRepository extends JpaRepository<PesapalTransaction, Long> {
-    PesapalTransaction findByPesapalOrderId(String pesapalOrderId);
     Optional<PesapalTransaction> findByMerchantReference(String merchantReference);
 }
